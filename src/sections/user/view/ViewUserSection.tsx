382842fb0fc51108fa174d0c444223a2355fc8ff
Nur Ahmadi <ahmadinur2202@gmail.com>
2023-06-23 13:35:53 +0800

    update view user

--- src/sections/user/view/ViewUserSection.tsx
+++ src/sections/user/view/ViewUserSection.tsx
@@ -24,7 +24,7 @@ const ViewUserSection = ({ user }: Props) => {
           alt="_image"
         />
       </div>
-      <div className="col-span-1 mr-10 min-h-[200px] rounded-lg border-[1px] border-gray-300 border-opacity-50 p-5 shadow-md md:col-span-2">
+      <div className="col-span-1 md:mr-10 min-h-[200px] rounded-lg border-[1px] border-gray-300 border-opacity-50 p-5 shadow-md md:col-span-2">
         <h1 className="text-[24px] font-bold">{user?.name}</h1>
         <p className="mt-2 text-[14px] text-gray-400">{user?.bio}</p>
       </div>
